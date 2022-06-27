22a50ff93f90c1cc9b408d91220107f3e03f5515
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-06-28 00:27:05 +0530

    Update apkdb.py

--- plugins/apkdb.py
+++ plugins/apkdb.py
@@ -1,35 +1,10 @@
-import re
-import uuid
-import socket
-import platform
-import time
-import math
-import json
-import string
-import traceback
-import psutil
-import asyncio
-import wget
-import motor.motor_asyncio
-import pymongo
-import aiofiles
-import datetime
 import os
-import random
 import logging
-from pyrogram.errors.exceptions.bad_request_400 import *
-from pyrogram.errors import *
-from pyrogram.types import *
-from helper.decorators import humanbytes
-from asyncio import *
-import requests
-from utils.database import Database
+
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.types import *
-from pyrogram.types import Message
 
-from info import START_MSG, CHANNELS, ADMINS, INVITE_MSG, DATABASE_URI, PRIVATE_LOG
+from info import START_MSG, CHANNELS, ADMINS, INVITE_MSG
 from utils import Media, unpack_new_file_id
 
 logger = logging.getLogger(__name__)
