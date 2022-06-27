762a56af384bf31e4c6f9832be6f89f50c57aa69
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-06-27 12:03:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,4 +37,4 @@ Here you can search files in inline mode. Just press following buttons and start
 
 START_MSG = environ.get('START_MSG', default_start_msg)
 SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
-INVITE_MSG = environ.get('INVITE_MSG', 'Please join @.... to use this bot')
+INVITE_MSG = environ.get('INVITE_MSG', 'Please join @EpicBotsSl to use this bot')
