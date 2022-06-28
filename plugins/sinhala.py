02926072c79b40912b536fbf59c71ca5ac8d9217
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-06-28 14:34:05 +0530

    Update sinhala.py

--- plugins/sinhala.py
+++ plugins/sinhala.py
@@ -2,3 +2,8 @@ STARTCMD = "🌼Choose language To Start bot!"
 
 COMMAND_LANGBTN = InlineKeyboardMarkup([[
       InlineKeyboardButton('සිංහල', callback_data="START_SI")
+      ],
+      [
+      InlineKeyboardButton('ENGLISH', callback_data="START_SI")
+      ])
+
