3ada167ae104839b9ed0d7fcc55913d6199758b9
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-06-28 16:34:53 +0530

    Update sinhala.py

--- sinhala.py
+++ sinhala.py
@@ -15,3 +15,27 @@ COMMAND_LANGBTN = InlineKeyboardMarkup([[
       [
       InlineKeyboardButton('ENGLISH 🇬🇧', callback_data="START_SI")
       ]])
+
+#=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•Epic Bots 2022© All Rights Resived•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=#
+#START_SI Callback Text & Btn
+
+SI_HELP = InlineKeyboardMarkup([[
+                InlineKeyboardButton('පෙර මෙනුවට පිවිසෙන්න', callback_data="HELP_BACK")
+            ]])
+
+SI_STARB = InlineKeyboardMarkup([[
+                InlineKeyboardButton('උදව්❔', callback_data="SIHELP_CLB")
+            ],
+            [
+                InlineKeyboardButton('👩‍💻අපගේ ඇප්ප් ඩේටාබේස් එක👩‍💻', url='https://t.me/EpicApkDatabase'),
+                InlineKeyboardButton('බොට් ඩිවලොපර්ස්', callback_data="Si_Devs")
+            ],
+            [
+                InlineKeyboardButton('</ᴇᴘɪᴄ ʙᴏᴛs <s/ʟ>🇱🇰', url='https://t.me/EpicBotsSl')
+            ],
+            [
+                InlineKeyboardButton('🔍ඇප් සර්ච් කරම්න🔄', switch_inline_query_current_chat=''),
+                InlineKeyboardButton('↗️ශෙයාර් කරපම්↗️', switch_inline_query='')
+            ]
+        ])
+
